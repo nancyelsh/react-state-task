eb90d7977e1218fc0347bb838fb2e666c3ab404d
Nancy El-Sharkawi <Nancy@Nancys-MacBook-Pro.local>
2021-11-28 05:24:19 +0300

    Imported Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import TripsList from "./Components/TripsList";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <TripsList />
+      <div className="container">
+        <div className="row">
+          <TripsList />
+        </div>
       </div>
     );
   }
